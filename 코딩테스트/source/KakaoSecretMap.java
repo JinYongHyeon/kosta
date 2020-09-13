95a836820e4a9593a7a876d4d18baf9918fe906b
JINYONGHYEON <yonghyun08@naver.com>
2020-09-13 22:28:25 +0900

    READE UPDATE

--- 코딩테스트/source/KakaoSecretMap.java
+++ 코딩테스트/source/KakaoSecretMap.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class KakaoSecretMap {
-	
+
 	public static HashMap<Integer,String> b(int n,int[] arr){
 		int index = 0;
 		HashMap<Integer, String> map = new HashMap<Integer, String>();
@@ -20,7 +20,7 @@ public class KakaoSecretMap {
 		 * StringBuilder을 이용하여 값을 담고 for loop을 통해 반복문을 돌리고 그 안에 while문을 돌려 10진수 2로 나머지 값
 		 * StringBuilder 추가하고 10진수 값이 2로 나눌때1 보다 작거나 같을경우 10진수 값 StringBuilder에 추가 후
 		 * while문을 빠져 나온다.
-		 * 
+		 *
 		 * StringBuilder 길이가 n보다 작을경우 0 추가
 		 */
 		for (int num : list) {
@@ -42,19 +42,19 @@ public class KakaoSecretMap {
 		}
 		return map;
 	}
-	
+
 	public static String[] a(int n, int[] arr1, int[] arr2) {
 		String[] answer = {};
-		
+
 		String good[] = new String[n];
 		HashMap<Integer,String> map = b(n,arr1);
 		HashMap<Integer,String> map2 = b(n,arr2);
-		
+
 		for (int j=0;j<map.size();j++) {
 			String name = map.get(j);
 			StringBuilder sb= new StringBuilder();
 			for (int i = 0; i < map.get(j).length(); i++) {
-				
+
 				if(!map.get(j).substring(i, i+1).contentEquals(map2.get(j).substring(i,i+1))) {
 					sb.append("#");
 				}else if(name.substring(i,i+1).equals("1")) {
@@ -62,34 +62,34 @@ public class KakaoSecretMap {
 				}else {
 					sb.append(" ");
 				}
-				
+
 			}
-			good[j] =sb.reverse().toString();			
+			good[j] =sb.reverse().toString();
 		}
 		answer = new String[good.length];
 		for(int i=0;i<answer.length;i++) {
 			answer[i] = good[i];
 		}
-			
+
 
 		return answer;
 	}
-	
+
 	public static  String[] c(int n, int[] arr1, int[] arr2) {
 		String[] answer = new String[n];
 		/*
 		   StringFormat형식  = %[argument_index$][flags][width]conversion
-		   % , conversion(s[문자열], d[정수], x[16진수], o[8진수], f[실수]) 필수  
-		   
+		   % , conversion(s[문자열], d[정수], x[16진수], o[8진수], f[실수]) 필수
+
 		       진수변환 함수
-		   Integer.toBinaryString(int) : String //2진수 
+		   Integer.toBinaryString(int) : String //2진수
 		   Integer.toOctalString(int) : String //8진수
 	       Integer.toHexString(int) : String //16진수
-	       
+
 	       replace(target , replacement) : String //특정문자 변경
 		*/
 		for(int i=0;i<n;i++) {
-			answer[i] = String.format("%"+n+"s", Integer.toBinaryString(arr1[i] | arr2[i]));
+			answer[i] = String.format("%"+n+"s", Integer.toBinaryString(arr1[i] | arr2[i])); // 비트 연산자 2진수 or 비교
 			answer[i] = answer[i].replace("1", "#");
 			answer[i] = answer[i].replace("0", " ");
 		}
